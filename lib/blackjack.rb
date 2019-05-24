a6534747e3a6c75f7f90794cbadc8f0147dec348
Becky Phan <beckyphan@live.com>
2019-05-24 22:09:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,6 +53,10 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  initial_round
+  if card_total < 21
+    hit
+    display_card_total
 end
     
