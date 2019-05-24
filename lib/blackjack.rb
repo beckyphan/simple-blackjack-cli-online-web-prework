142a91790c1d0c3d0e8d232e356aecbe2d9ef8f6
Becky Phan <beckyphan@live.com>
2019-05-24 22:06:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,9 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-  first = deal_card
-  second = deal_card
-  @card_total = first + second
+  @card_total = deal_card + deal_card
   display_card_total(card_total)
   return card_total
 end
